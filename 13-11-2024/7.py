a4c29e5c7023c59b0d71ba79b01991141105482e
Chavan-Gourav <gouravchavan8276@gmail.com>
2024-11-13 03:08:34 +0000

    added 7.py file

---
+++ 13-11-2024/7.py
@@ -0,0 +1,6 @@
+# extend the list with another list of Cars.
+
+Cars = ["Audi", "BMW", "Hyundai", "Mustang", "Maruti-Suzuki", "Mahindra"]
+More_Cars = ["Tesla", "Nisan"]
+Cars.extend(More_Cars)
+print(Cars)
