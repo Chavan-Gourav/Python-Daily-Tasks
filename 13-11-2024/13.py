a1873daf4f9fed4e2df8c26c429601cf996c30ae
Chavan-Gourav <gouravchavan8276@gmail.com>
2024-11-13 03:41:24 +0000

    added 13.py fiel

---
+++ 13-11-2024/13.py
@@ -0,0 +1,5 @@
+# sort the original list of Cars in descending order.
+
+Cars = ["Audi", "BMW", "Hyundai","BMW", "Mustang", "Maruti-Suzuki", "BMW", "Mahindra"]
+Cars.sort(reverse = True)
+print(Cars)
