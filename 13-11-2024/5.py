0b31e919389803cb56e9f810b2886f7cb92d4520
Chavan-Gourav <gouravchavan8276@gmail.com>
2024-11-13 03:06:12 +0000

    added 6py file

---
+++ 13-11-2024/5.py
@@ -0,0 +1,5 @@
+#remove 'Audi' from the list.
+
+Cars = ["Audi", "BMW", "Hyundai", "Maruti-Suzuki", "Mahindra"]
+Cars.remove("Audi")
+print(Cars)
---
+++ 13-11-2024/6.py
@@ -0,0 +1,7 @@
+#find the index of "Mustang".
+
+Cars = ["Audi", "BMW", "Hyundai", "Mustang", "Maruti-Suzuki", "Mahindra"]
+
+Index_value = Cars.index("Mustang")
+print(Index_value)
+
