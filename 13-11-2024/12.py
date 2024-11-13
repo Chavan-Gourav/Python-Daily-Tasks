532a36b8720c7563788a755e806613d1bb0cfb91
Chavan-Gourav <gouravchavan8276@gmail.com>
2024-11-13 03:39:19 +0000

    added 13.py file

---
+++ 13-11-2024/12.py
@@ -0,0 +1,16 @@
+#check if list is empty and clear it if it is not.
+
+Cars = ["Audi", "BMW", "Hyundai","BMW", "Mustang", "Maruti-Suzuki", "BMW", "Mahindra"]
+
+if not Cars:
+    print("Cars list is empty")
+else:
+    Cars.clear()
+    print("CLeared Cars list")
+
+# if len(Cars) == 0:
+#     print("Cars list is empty")
+
+# else:
+#     Cars.clear()
+#     print("Cleared Cars list")
